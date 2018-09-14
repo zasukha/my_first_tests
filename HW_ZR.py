3b7be6c1d53a88fb16eee5dd4b6a3cab98f733e2
zasukha <zasukha.rv@ukr.net>
2018-09-14 18:04:28 +0300

    fix bugs, add new steps, add comments to HW_ZR

--- HW_ZR.py
+++ HW_ZR.py
@@ -1,7 +1,12 @@
+#this test for login, how wiki searh requests for Ukraine and logout flow. Request_data.json is need to run this test.
+
 import unittest
 
 from ddt import ddt, file_data
 from selenium import webdriver
+
+user_name = "zas_rv" #input your login
+password = "forester777" #input your password
 
 @ddt
 class HW_ZR(unittest.TestCase):
@@ -12,19 +17,25 @@ class HW_ZR(unittest.TestCase):
         self.driver.maximize_window()
 
     @file_data('request_data.json')
-    def test_login_account_email_validation(self, value):
-        self.driver.get("https://en.wikipedia.org/wiki/Main_Page")
-        self.driver.find_element_by_css_selector('a[accesskey="o"]').click()
-        self.driver.find_element_by_css_selector('input[name="wpName"]').send_keys('zas_rv')
-        self.driver.find_element_by_css_selector('input[id="wpPassword1"]').send_keys('forester777')
-        self.driver.find_element_by_id("wpLoginAttempt").click()
+    def test_login_search_logout_flow(self, value):
 
-        #self.driver.find_element_by_css_selector('a[aria-label="Close"]').click()
+        self.driver.get("https://en.wikipedia.org/wiki/Main_Page")
+        self.assertTrue(self.driver.find_element_by_css_selector('a[class="mw-wiki-logo"]').is_displayed())
+        self.driver.find_element_by_css_selector('a[accesskey="o"]').click()
+
+        self.driver.find_element_by_css_selector('input[name="wpName"]').send_keys(user_name)
+        self.driver.find_element_by_css_selector('input[id="wpPassword1"]').send_keys(password)
+        self.driver.find_element_by_id("wpLoginAttempt").click()
+        self.assertTrue(self.driver.find_element_by_css_selector('a[title="Log out"]').is_displayed())
 
         self.driver.find_element_by_css_selector('input[id="searchInput"]').send_keys(value)
         self.driver.find_element_by_css_selector('input[ id="searchButton"]').click()
         self.driver.find_element_by_css_selector('a[title="Ukraine"]').click()
         self.assertTrue(self.driver.find_element_by_css_selector('img[alt="Flag of Ukraine"]').is_displayed())
+        self.assertTrue(self.driver.find_element_by_css_selector('a[title="Ukrainian language"]').is_displayed())
+
+        self.driver.find_element_by_css_selector('a[title="Log out"]').click()
+        self.assertTrue(self.driver.find_element_by_css_selector('a[title="This is a special page which you cannot edit"]').is_displayed())
 
     def tearDown(self):
         self.driver.quit()
