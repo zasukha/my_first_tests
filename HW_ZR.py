95b5419e3528acc50d2002e8c12c7fc597112396
zasukha <zasukha.rv@ukr.net>
2018-09-14 19:51:22 +0300

    correct code and css selectors as Dimytro wish

--- HW_ZR.py
+++ HW_ZR.py
@@ -1,12 +1,15 @@
-#this test for login, how wiki searh requests for Ukraine and logout flow. Request_data.json is need to run this test.
+"""
+This is test for login, how wiki search requests for Ukraine and logout flow. Request_data.json is need to run this test.
+"""
 
 import unittest
 
 from ddt import ddt, file_data
 from selenium import webdriver
 
-user_name = "zas_rv" #input your login
-password = "forester777" #input your password
+user_name = "zas_rv"  # input your login
+password = "forester777"  # input your password
+
 
 @ddt
 class HW_ZR(unittest.TestCase):
@@ -18,24 +21,23 @@ class HW_ZR(unittest.TestCase):
 
     @file_data('request_data.json')
     def test_login_search_logout_flow(self, value):
-
         self.driver.get("https://en.wikipedia.org/wiki/Main_Page")
         self.assertTrue(self.driver.find_element_by_css_selector('a[class="mw-wiki-logo"]').is_displayed())
-        self.driver.find_element_by_css_selector('a[accesskey="o"]').click()
+        self.driver.find_element_by_css_selector('[id="pt-login"]').click()
 
         self.driver.find_element_by_css_selector('input[name="wpName"]').send_keys(user_name)
         self.driver.find_element_by_css_selector('input[id="wpPassword1"]').send_keys(password)
         self.driver.find_element_by_id("wpLoginAttempt").click()
-        self.assertTrue(self.driver.find_element_by_css_selector('a[title="Log out"]').is_displayed())
+        self.assertTrue(self.driver.find_element_by_css_selector('li[id="pt-logout"]').is_displayed())
 
         self.driver.find_element_by_css_selector('input[id="searchInput"]').send_keys(value)
-        self.driver.find_element_by_css_selector('input[ id="searchButton"]').click()
+        self.driver.find_element_by_css_selector('input[id="searchButton"]').click()
         self.driver.find_element_by_css_selector('a[title="Ukraine"]').click()
         self.assertTrue(self.driver.find_element_by_css_selector('img[alt="Flag of Ukraine"]').is_displayed())
-        self.assertTrue(self.driver.find_element_by_css_selector('a[title="Ukrainian language"]').is_displayed())
+        self.assertTrue(self.driver.find_element_by_css_selector('a[href="/wiki/Shche_ne_vmerla_Ukraina"]').is_displayed())
 
-        self.driver.find_element_by_css_selector('a[title="Log out"]').click()
-        self.assertTrue(self.driver.find_element_by_css_selector('a[title="This is a special page which you cannot edit"]').is_displayed())
+        self.driver.find_element_by_css_selector('li[id="pt-logout"]').click()
+        self.assertTrue(self.driver.find_element_by_css_selector('li[id="ca-nstab-special"]').is_displayed())
 
     def tearDown(self):
         self.driver.quit()
